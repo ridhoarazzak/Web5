50c63ec422143b9a3f45e5209111234d49d8d0dc
dhooa <arazza1693@gmail.com>
2025-06-29 02:53:09 +0700

    Create next.config.js

---
+++ next.config.js
@@ -0,0 +1,12 @@
+// next.config.js
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  // Tambahkan konfigurasi khusus Next.js di sini
+  // Contoh: untuk mengizinkan gambar dari domain tertentu jika Anda menggunakan URL eksternal
+  // images: {
+  //   domains: ['example.com'],
+  // },
+};
+
+module.exports = nextConfig;
